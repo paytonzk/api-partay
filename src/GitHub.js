3134ef5a3452f9029d0ab4208ee3361bdff73605
Zach Payton <paytonzk@rose-hulman.edu>
2017-07-11 15:21:28 -0400

    Added a form and input and tied the value of input to state

--- src/GitHub.js
+++ src/GitHub.js
@@ -2,13 +2,21 @@ import React, {Component} from 'react'
 import './GitHub.css'
 
 class GitHub extends Component{
+    state = {
+        username: null
+    }
+
+    handleChange = (ev) => {
+        this.setState({username: ev.target.value})
+    }
+
     render(){
         return(
         <div className="github">
             <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github logo"/>
             <form>
                 <div>
-                    <input type="text" />
+                    <input type="text" value={this.state.username} onChange={this.handleChange}/>
                     <div>
                         <button type="submit">Look up Github user</button>
                     </div>
