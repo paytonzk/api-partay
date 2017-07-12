a9d17a67362f458525379a49b09e7d7ca5029d91
Zach Payton <paytonzk@rose-hulman.edu>
2017-07-11 20:29:36 -0700

    Adding homework and nasa stuff

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -1,35 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './GithubUser.css'
-import {Route} from 'react-router-dom'
 
-class GitHub extends Component{
-    constructor(props){
+class GithubUser extends Component {
+  constructor(props) {
     super(props)
 
     this.state = {
-        user: {}
+      user: {}
     }
 
     this.fetchUserData(props)
-    }
+  }
 
-    fetchUserData = () => {
-        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
-        .then(response => response.json())
-        .then(user => this.setState({user }))
-    }
+  fetchUserData = (props) => {
+    fetch(`https://api.github.com/users/${props.match.params.username}`)
+      .then(response => response.json())
+      .then(user => this.setState({ user }))
+  }
 
-    render(){
-        const {user} = this.state
-        return(
-            <div className="github-user">
-                <img src={user.avatar_url} alt="github user avatar"/>
-                <h2>{user.login}</h2>
-                <h3>Followers: {user.followers}</h3>
-                <h3>Following: {user.following}</h3>
-                <h3>Location: {user.location}</h3>
-                <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
-            </div>
-        )
+  componentWillReceiveProps(nextProps) {
+    const locationChanged = nextProps.location !== this.props.location
+    if (locationChanged) {
+      this.fetchUserData(nextProps)
     }
+  }
+
+  render() {
+    const { user } = this.state
+    return (
+      <div className="github-user">
+        <img src={user.avatar_url} alt="github user avatar" />
+        <h2>{user.login}</h2>
+        <h3>followers: {user.followers}</h3>
+        <h3>following: {user.following}</h3>
+        <h3>location: {user.location}</h3>
+        <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
+      </div>
+    )
+  }
 }
+export default GithubUser
